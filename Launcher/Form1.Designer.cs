b3cf3eaf1aab0f40c772defe9ba2a14d788c74ac
cescm <jcescm@gmail.com>
2019-03-31 22:26:44 +0200

    Initial Commit

---
+++ Launcher/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(578, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Instalar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(690, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AccessibleName = "";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Normal",
+            "Silenciosa"});
+            this.comboBox1.Location = new System.Drawing.Point(671, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(118, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.Tag = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tipo de Instalación";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Aplicación";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Versión";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(108, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Desarrollado por: ";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(115, 8);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(132, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Juan Carlos Escobosa";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(389, 8);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Espacio libre en C: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(524, 8);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(530, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 13);
+            this.label7.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(300, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(46, 13);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Tamaño";
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Location = new System.Drawing.Point(6, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(468, 728);
+            this.panel1.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(15, 29);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Aplicación";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(0, 736);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(888, 30);
+            this.panel2.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(480, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(164, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Tamaño total seleccionado:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Red;
+            this.label10.Location = new System.Drawing.Point(650, 174);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(164, 13);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Tamaño total seleccionado:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(578, 264);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 169);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(888, 766);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Launcher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Launcher/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sourceDirectory = @"c:\prueba";
+            
+            List<CheckBox> CheckBoxes = new List<CheckBox>();
+            List<Label> LabelsVersiones = new List<Label>();
+            List<Label> LabelsSizes = new List<Label>();
+
+            comboBox1.SelectedIndex = comboBox1.Items.IndexOf("Normal");
+            
+            try
+            {
+                //insertamos primero los checkboxes
+                int cont = 0;
+                
+                //este lo usaremos cuando lo metamos en la carpeta definitica, las APPS irán en una carpeta llamada Apps
+                //string[] filePaths = Directory.GetFiles(@"c:\prueba\");
+
+                string[] filePaths = Directory.GetDirectories(@"C:\prueba");
+
+                //este lo usaremos cuando lo metamos en la carpeta definitica, las APPS irán en una carpeta llamada Apps
+                //string folder = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + @"\Apps\";
+                //string[] filePaths = Directory.GetDirectories(folder);
+
+                string[] files = Directory.GetFiles(@"c:\prueba\", "*.exe", SearchOption.AllDirectories);
+                
+                //este lo usaremos cuando lo metamos en la carpeta definitica, las APPS irán en una carpeta llamada Apps
+                //string[] files = Directory.GetFiles(folder, "*.exe", SearchOption.AllDirectories);
+
+                foreach (string currentFilePath in filePaths)
+                {
+                    
+                    cont++;
+                    
+                    CheckBox cb = new CheckBox();
+                    cb.Location = new System.Drawing.Point(15, 30 + cont * 30);
+                    //cb.Text = currentFilePath.Substring(sourceDirectory.Length+6);
+                    cb.Text = currentFilePath.Substring(sourceDirectory.Length + 1);
+                    cb.CheckAlign = ContentAlignment.MiddleLeft;
+                    cb.TextAlign = ContentAlignment.MiddleLeft;
+                    cb.AutoSize = true;
+                    CheckBoxes.Add(cb);
+                                     
+                }
+                //ahora vamos a insertar las versiones de los archivos
+                cont = 0;
+                foreach (string currentFile in files)
+                {
+                    cont++;
+                    Label lb = new Label();
+                    Label lb2 = new Label();
+                    lb.Location = new System.Drawing.Point(200, 30 + cont * 30);
+                    lb2.Location = new System.Drawing.Point(300, 30 + cont * 30);
+                    var pathToFile = Path.GetFullPath(currentFile);
+                    var versionInfo = FileVersionInfo.GetVersionInfo(pathToFile);
+                    if (versionInfo.ProductVersion != "")
+                    {
+                        lb.Text = versionInfo.ProductVersion;
+                    }else if (versionInfo.FileVersion != "")
+                    {
+                        lb.Text = versionInfo.FileVersion;
+                    }else
+                    {
+                        lb.Text = "N/A";
+                    }
+                    
+                    
+                    FileInfo f = new FileInfo(pathToFile);
+                    var sizeInfo = f.Length;
+                    lb2.Text = Math.Round(Convert.ToDouble(sizeInfo/1048576),0).ToString() + "Mb";
+                    LabelsVersiones.Add(lb);
+                    LabelsSizes.Add(lb2);
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+                Console.Write(ex.Message);
+                MessageBox.Show(ex.Message);
+            }
+            
+            foreach (CheckBox c in CheckBoxes) {
+                //this.Controls.Add(c);
+                panel1.Controls.Add(c);
+            }
+            foreach (Label l in LabelsVersiones)
+            {
+                //this.Controls.Add(l);
+                panel1.Controls.Add(l);
+            }
+            foreach (Label l2 in LabelsSizes)
+            {
+                //this.Controls.Add(l2);
+                panel1.Controls.Add(l2);
+            }
+
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady && drive.Name == @"C:\")
+                {
+                    int espacio = (int)Math.Round(Convert.ToDouble(drive.TotalFreeSpace / 1048576), 0);
+                    label6.Text = espacio.ToString() + "Mb";
+                }
+ 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:jcescm@gmail.com");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                foreach (CheckBox control in panel1.Controls.OfType<CheckBox>())
+                {
+                    control.Checked = true;
+                }
+            }
+            else
+            {
+                foreach (CheckBox control in panel1.Controls.OfType<CheckBox>())
+                {
+                    control.Checked = false;
+                }
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int modoInstalacion = comboBox1.SelectedIndex;
+            foreach (CheckBox control in panel1.Controls.OfType<CheckBox>())
+            {
+                if (control.Checked == true){
+                    //modo de instalación normal
+                    if (modoInstalacion == 0)
+                    {
+                        Process p = new Process();
+                        string[] fileName = Directory.GetFiles(@"c:\prueba\" + control.Text, "*.exe");
+                        MessageBox.Show(fileName[0]);
+                        p.StartInfo.FileName = fileName[0];
+                        p.Start();
+                        p.WaitForExit();
+                    }
+                    else//modo de instalación silenciosa
+                    {
+                       
+                        MessageBox.Show(IsAdministrator().ToString());
+                        ProcessStartInfo startInfo = new ProcessStartInfo();
+                        startInfo.CreateNoWindow = true;
+                        startInfo.UseShellExecute = false;
+                        string[] fileName = Directory.GetFiles(@"c:\prueba\" + control.Text, "*.exe");
+                        startInfo.Verb = "runas";
+                        startInfo.FileName = fileName[0];
+                        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                        startInfo.Arguments = "/S";
+                        using (Process exeProcess = Process.Start(startInfo))
+                        {
+                            exeProcess.WaitForExit();
+                           
+                        }
+
+                    }
+                }
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"c:\prueba\img\2.jpg");
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"c:\prueba\img\1.jpg");
+        }
+    }
+}
